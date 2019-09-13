5ee359b9fb2e5ab7ca5ecd06fa262b4e5edb4c1b
houkong <37408284+houkong@users.noreply.github.com>
2019-09-13 18:19:52 +0200

    Version 1.7.2
    Cosmetic changes for plotting

--- Plot.py
+++ Plot.py
@@ -1,7 +1,14 @@
 import matplotlib.pyplot as plt
+import matplotlib
 import os
+from matplotlib.pyplot import figure
+figure(num=None, figsize=(8,8), dpi=80, facecolor='w', edgecolor='k')
+font = {'size'   : 22}
+matplotlib.rc('font', **font)
 
 SHOW = False
+WIDTH = 4
+
 
 class Plot:
     def __init__(self, element=None):
@@ -25,7 +32,7 @@ class Plot:
         for i in range(self.size()):
             data_i = self.data[i]
             assert len(data_i.time) == len(data_i.theta)
-            plt.plot(data_i.time, data_i.theta, label=data_i.name)
+            plt.plot(data_i.time, data_i.theta, label=data_i.name, linewidth=WIDTH)
         plt.xlabel('time [s]')
         plt.ylabel('theta [radians]')
         plt.legend()
@@ -34,6 +41,7 @@ class Plot:
         if not os.path.exists("plots"):
             os.mkdir("plots")
         path = "plots/t_theta_" + self.title if self.title else "plots/t_theta.png"
+        plt.tight_layout()
         plt.savefig(path)
         if SHOW:
             plt.show()
@@ -44,7 +52,7 @@ class Plot:
         for i in range(self.size()):
             data_i = self.data[i]
             assert len(data_i.time) == len(data_i.energy)
-            plt.plot(data_i.time, data_i.energy, label=data_i.name)
+            plt.plot(data_i.time, data_i.energy, label=data_i.name, linewidth=WIDTH)
         plt.xlabel('time [s]')
         plt.ylabel('energy [J]')
         plt.legend()
@@ -53,6 +61,7 @@ class Plot:
         if not os.path.exists("plots"):
             os.mkdir("plots")
         path = "plots/t_energy_" + self.title if self.title else "plots/t_energy.png"
+        plt.tight_layout()
         plt.savefig(path)
         if SHOW:
             plt.show()
@@ -63,7 +72,7 @@ class Plot:
         for i in range(self.size()):
             data_i = self.data[i]
             assert len(data_i.theta) == len(data_i.omega)
-            plt.plot(data_i.theta, data_i.omega, label=data_i.name)
+            plt.plot(data_i.theta, data_i.omega, label=data_i.name, linewidth=WIDTH)
         plt.xlabel('theta [radians]')
         plt.ylabel('omega [radians/s]')
         plt.legend()
@@ -72,6 +81,7 @@ class Plot:
         if not os.path.exists("plots"):
             os.mkdir("plots")
         path = "plots/theta_omega_" + self.title if self.title else "plots/theta_omega.png"
+        plt.tight_layout()
         plt.savefig(path)
         if SHOW:
             plt.show()
--- main.py
+++ main.py
@@ -8,11 +8,17 @@ START = 0
 STOP = 20
 STEP = 0.1
 
-_simple_euler1 = Plot(simple_euler(THETA0, OMEGA0, start=START, stop=STOP, step=0.01))
-_simple_euler2 = Plot(simple_euler(THETA0, OMEGA0, start=START, stop=STOP, step=0.1))
-_simple_euler3 = Plot(simple_euler(THETA0, OMEGA0, start=START, stop=STOP, step=1))
 
-plot1 = _simple_euler1 + _simple_euler2 + _simple_euler3
+def format_name(step):
+    return "∆t = " + str(step) + "s"
+
+
+_simple_euler1 = Plot(simple_euler(THETA0, OMEGA0, start=START, stop=STOP, step=0.02, name=format_name(0.02)))
+_simple_euler2 = Plot(simple_euler(THETA0, OMEGA0, start=START, stop=STOP, step=0.2, name=format_name(0.2)))
+_simple_euler3 = Plot(simple_euler(THETA0, OMEGA0, start=START, stop=STOP, step=0.4, name=format_name(0.4)))
+_analytic = Plot(analytic(THETA0, OMEGA0, stop=STOP, step=STEP))
+
+plot1 = _simple_euler1 + _simple_euler2 + _simple_euler3 + _analytic
 plot1.title = "simple_euler"
 plot1.plot_t_theta()
 plot1.plot_t_energy()
@@ -30,19 +36,21 @@ plot2.plot_t_energy()
 plot2.plot_theta_omega()
 
 
-_runge_kutta1 = Plot(rk4(THETA0, OMEGA0, start=START, stop=STOP, step=0.1))
-_runge_kutta2 = Plot(rk4(THETA0, OMEGA0, start=START, stop=STOP, step=0.01))
-_runge_kutta3 = Plot(rk4(THETA0, OMEGA0, start=START, stop=STOP, step=0.04))
+_runge_kutta1 = Plot(rk4(THETA0, OMEGA0, start=START, stop=STOP, step=0.1, name=format_name(0.1)))
+_runge_kutta2 = Plot(rk4(THETA0, OMEGA0, start=START, stop=STOP, step=0.01, name=format_name(0.01)))
+_runge_kutta3 = Plot(rk4(THETA0, OMEGA0, start=START, stop=STOP, step=0.04, name=format_name(0.04)))
+_analytic = Plot(analytic(THETA0, OMEGA0, stop=STOP, step=STEP))
 
-plot3 = _runge_kutta1 + _runge_kutta2 + _runge_kutta3
+plot3 = _runge_kutta1 + _runge_kutta2 + _runge_kutta3 + _analytic
 plot3.title = "rk4"
 plot3.plot_t_theta()
 
-_euler_cromer1 = Plot(euler_cromer(THETA0, OMEGA0, start=START, stop=STOP, step=0.01))
-_euler_cromer2 = Plot(euler_cromer(THETA0, OMEGA0, start=START, stop=STOP, step=0.1))
-_euler_cromer3 = Plot(euler_cromer(THETA0, OMEGA0, start=START, stop=STOP, step=0.04))
+_euler_cromer1 = Plot(euler_cromer(THETA0, OMEGA0, start=START, stop=STOP, step=0.2, name=format_name(0.2)))
+_euler_cromer2 = Plot(euler_cromer(THETA0, OMEGA0, start=START, stop=STOP, step=0.8, name=format_name(0.8)))
+_euler_cromer3 = Plot(euler_cromer(THETA0, OMEGA0, start=START, stop=STOP, step=1, name=format_name(1)))
+_analytic = Plot(analytic(THETA0, OMEGA0, stop=STOP, step=STEP))
 
 
-plot4 = _euler_cromer1 + _euler_cromer2 + _euler_cromer3
+plot4 = _euler_cromer1 + _euler_cromer2 + _euler_cromer3 + _analytic
 plot4.title = "euler_cromer"
 plot4.plot_t_theta()
